b4829cfe24f1374f04b3ac7456e54e67e829e6b6
barinov3d <barinov3d@gmail.com>
2018-06-11 14:04:21 +0400

    Adds covarage for jUnit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,6 @@
  */
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.print("Hi!");
-    }
-
     public String concatenate(String one, String two){
         return one + two;
     }
